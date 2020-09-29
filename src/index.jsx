b063999145d1ad4a3d9746c10e9af26221e8dfa4
smlnx <alexander.is.dev@gmail.com>
2020-09-29 14:07:58 +0300

    rename(./src/index.jsx) file extension

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import './assets/scss/main.scss';
 
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import TodoBox from './components/TodoBox.jsx';
 
 ReactDOM.render(
