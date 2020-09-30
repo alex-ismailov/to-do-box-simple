875ddaf792c9a622d37654c5af1abf44e85849bf
smlnx <alexander.is.dev@gmail.com>
2020-10-01 00:31:05 +0300

    change(./myExpressServer/bin/start.js) port number

--- myExpressServer/bin/start.js
+++ myExpressServer/bin/start.js
@@ -1,6 +1,6 @@
 import app from '../application.js';
 
-const port = 8090;
+const port = 8080;
 app().listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server has been started, at ${new Date()}`);
